227fe2f023249a815e5c199a6cea8801d79bf1c2
Samrendra Kumar <154054931+Samyrd@users.noreply.github.com>
2026-01-21 23:15:45 +0530

    Update calculator.js

--- JavaScript/calculator.js
+++ JavaScript/calculator.js
@@ -1,27 +1,44 @@
-// program for a simple calculator
+// // program for a simple calculator
 
-// take the operator input
-const operator = prompt('Enter operator ( either +, -, * or / ): ');
+// // take the operator input
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
 
-// take the operand input
-const number1 = parseFloat(prompt('Enter first number: '));
-const number2 = parseFloat(prompt('Enter second number: '));
+// // take the operand input
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
 
-let result;
+// let result;
 
-// using if...else if... else
-if (operator == '+') {
-    result = number1 + number2;
+// // using if...else if... else
+// if (operator == '+') {
+//     result = number1 + number2;
+// }
+// else if (operator == '-') {
+//     result = number1 - number2;
+// }
+// else if (operator == '*') {
+//     result = number1 * number2;
+// }
+// else {
+//     result = number1 / number2;
+// }
+
+// // display the result
+// console.log(`${number1} ${operator} ${number2} = ${result}`);
+
+
+//Iterating through the array elements..
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fLen = fruits.length;
+
+let text = "<ul>";
+for (let i = 0; i < fLen; i++) {
+  text += "<li>" + fruits[i] + "</li>";
 }
-else if (operator == '-') {
-    result = number1 - number2;
-}
-else if (operator == '*') {
-    result = number1 * number2;
-}
-else {
-    result = number1 / number2;
-}
+text += "</ul>";
 
-// display the result
-console.log(`${number1} ${operator} ${number2} = ${result}`);
+console.log(text);
+
+
+
+
